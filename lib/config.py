2b26f1e315009d645578879357766a9bbfaecf56
dilaouid <dilaouid@student.42.fr>
2025-02-25 17:10:51 +0100

    fix config

--- lib/config.py
+++ lib/config.py
@@ -1,4 +1,13 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read('config.ini')
+config = {
+    "FFMPEG": {
+        "ENCODER": "libx264",
+        # "ENCODER": "libx265", or "libx264" or "libvpx-vp9" etc...
+        # For GPU encoding, use "h264_nvenc" or "hevc_nvenc" for Nvidia cards
+        "CRF": "23"
+        # "CRF": Constant Rate Factor, 0 = lossless, 51 = worst quality
+    },
+    "FONT": {
+        "Size": "24",    # Taille de police pour les sous-titres
+        "Name": "Arial"  # Nom de la police
+    }
+}
